f6112028c0d5405cc33a79cbced62164003b8000
Eric Mendez <eric.takashi.mendez@gmail.com>
2023-04-15 11:35:43 -0400

    add new stop_mc lambda and add to stack template -- also reduce noise in stack

---
+++ lambdas/stop_mc.py
@@ -0,0 +1,24 @@
+import boto3
+import os
+from datetime import datetime, timedelta
+
+def handler(event, context):
+    ec2 = boto3.resource('ec2')
+    instance_id = os.environ.get('INSTANCE_ID')
+    instance = ec2.Instance(instance_id)
+    
+    launch_time = instance.launch_time
+    now = datetime.now(launch_time.tzinfo)
+    elapsed_time = now - launch_time
+    
+    if elapsed_time >= timedelta(hours=6):
+        instance.stop()
+        return {
+            'statusCode': 200,
+            'body': 'Instance stopped'
+        }
+    else:
+        return {
+            'statusCode': 200,
+            'body': 'Instance not yet eligible for stopping'
+        }
